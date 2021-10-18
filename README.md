eefafcf29883a18cb2d8f7052634f467c5e352ac
xiaotuan <583168425@qq.com>
2021-10-18 11:25:03 +0800

    实现批量重名程序

---
+++ README.md
@@ -0,0 +1,42 @@
+# BatchRename
+
+`BatchRename` 程序用于将文件夹中一级目录下的所有文件重命名为指定格式的文件名。
+
+> 注意：`BatchRename` 程序通过文件名中最后一个 `.` 字符来分割文件名和文件后缀名，如果要重命名的文件后缀名有多个 `.` 字符，则会导致重命名后缀出现错误。例如：`Linux` 系统中的 `test.tar.gz` 文件，重命名后将可能变成 `test.gz`。
+因此该程序只针对文件后缀名中只有一个 `.` 的文件。例如：`text.png`。
+
+# 使用方法
+
+`BatchRename` 需要使用 `-P` 或 `--path` 参数指定重命名文件所在的文件夹路径，例如（这里假设 `BatchRename.exe` 程序位于当前目录命令行环境下，以下相同）：
+
+```shell
+$ ./BatchRename.exe -P C:/testdir
+```
+
+> 注意：这里没有指定文件名前缀和文件编号，重命名文件时，文件编号起始值为 0，没有前缀字符。
+
+如果希望重命名后给每个文件添加前缀，可以使用 `-p` 或 `-prefix` 参数指定文件名前缀，例如：
+
+```shell
+$ ./BatchRename.exe -P C:/testdir -p animation_
+```
+
+如果希望指定文件的起始编号，可以使用 `-n` 或 `--number` 参数指定文件起始编号，例如：
+
+```shell
+$ ./BatchRename.exe -P C:/testdir -p animation_ -n 100
+```
+
+> 注意：文件起始编号必须是整数。
+
+也可以直接运行 `BatchRename` 程序或使用 `-h` 或 `--help` 参数来查看 `BatchRename` 程序的使用说明，例如：
+
+```shell
+$ BatchRename.exe
+
+使用方法:
+    -P / --path <path> 指定需要重命名文件所在的文件夹路径
+    -p / --Prefix <string> 指定重命名文件名前缀
+    -n / --number <int> 指定重命名起始编号
+
+```
---
+++ src/com/qty/batchrename/BatchRename.java
@@ -0,0 +1,196 @@
+package com.qty.batchrename;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+
+public class BatchRename {
+	
+	private static final boolean DEBUG = false;
+
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			showHelper();
+			return;
+		}
+		
+		HashMap<String, String> argMaps = parseArgs(args);
+		
+		if (argMaps == null) {
+			return;
+		}
+		
+		printDebugMessage("参数个数: " + argMaps.size());
+		if (argMaps.size() == 1 && argMaps.containsKey("help")) {
+			showHelper();
+			return;
+		}
+		
+		if (!argMaps.containsKey("path")) {
+			printErrorMessage("需要传递重命名文件的目录路径");
+			return;
+		}
+		
+		String path = argMaps.get("path");
+		String userDir = System.getProperty("user.dir");
+		printDebugMessage("当前目录路径: " + userDir);
+		File dirFile = new File(argMaps.get("path"));
+		printDebugMessage("传入的路径是否是绝对路径: " + dirFile.isAbsolute());
+		if (!dirFile.isAbsolute()) {
+			dirFile = new File(userDir + File.separator + path);
+		}
+		
+		if (!dirFile.isDirectory()) {
+			printErrorMessage(dirFile.getPath() + " 不是一个目录");
+			return;
+		}
+		
+		File[] originFiles = dirFile.listFiles();
+		ArrayList<File> files = new ArrayList<File>();
+		for (int i = 0; i < originFiles.length; i++) {
+			if (originFiles[i].exists() && originFiles[i].isFile()) {
+				files.add(originFiles[i]);
+			}
+		}
+		
+		if (DEBUG) {
+			if (files.size() == 0) {
+				System.out.println(dirFile.getAbsolutePath() + " 目录中没有找到文件。");
+				return;
+			} else {
+				printArrayList("目录中包含如下文件: ", files);
+			}
+		}
+		
+		printDebugMessage("排序文件...");
+		Collections.sort(files, new Comparator<File>() {
+
+			@Override
+			public int compare(File o1, File o2) {
+				
+				String name1 = o1.getName().substring(0, o1.getName().indexOf("."));
+				String name2 = o2.getName().substring(0, o1.getName().indexOf("."));
+				printDebugMessage("Name 1: " + name1 + ", Name 2: " + name2);
+				if (name1.length() == name2.length()) {
+					return name1.compareTo(name2);
+				} else {
+					return name1.length() - name2.length();
+				}
+			}
+		});
+		
+		printArrayList("排序后的文件列表: ", files);
+		
+		String prefix = "";
+		if (argMaps.containsKey("prefix")) {
+			prefix = argMaps.get("prefix");
+			System.out.println("重命名文件名前缀为: " + prefix);
+		}
+		int startNumber = 0;
+		if (argMaps.containsKey("number")) {
+			startNumber = Integer.parseInt(argMaps.get("number"));
+			System.out.println("文件起始编号为: " + startNumber);
+		}
+		printDebugMessage("开始重命名文件。。。");
+		File file = null;
+		int maxWidth = ((files.size() + startNumber) + "").length();
+		for (int i = 0; i < files.size(); i++) {
+			file = files.get(i);
+			String fileName = String.format("%s%0" + maxWidth + "d%s", 
+					prefix, startNumber, file.getName().substring(file.getName().lastIndexOf(".")));
+			File renameFile = new File(file.getParentFile().getAbsolutePath() + File.separator + fileName);
+			printDebugMessage(file.getName() + " ---> " + renameFile.getName());
+			file.renameTo(renameFile);
+			startNumber++;
+		}
+		
+		printArrayList("重命名后的文件列表: ", files);
+		
+		System.out.println("完成。。。");
+	}
+	
+	private static void showHelper() {
+		System.out.println("");
+		System.out.println("使用方法:");
+		System.out.println("    -P / --path <path> 指定需要重命名文件所在的文件夹路径");
+		System.out.println("    -p / --Prefix <string> 指定重命名文件名前缀");
+		System.out.println("    -n / --number <int> 指定文件起始编号");
+		System.out.println("");
+	}
+
+	private static HashMap<String, String> parseArgs(String[] args) {
+		printDebugMessage("参数: " + Arrays.toString(args));
+		if (args == null || args.length == 0) {
+			printErrorMessage("未设置参数");
+			return null;
+		}
+		HashMap<String, String> results = new HashMap<>();
+		String key = null;
+		for (int i = 0; i < args.length; i++) {
+			printDebugMessage("参数(" + i + "): " + args[i]);
+			switch (args[i]) {
+				case "-P":
+				case "--path":
+					key = "path";
+					break;
+					
+				case "-h":
+				case "--help":
+					results.put("help", "help");
+					break;
+					
+				case "-p":
+				case "--Prefix":
+					key = "prefix";
+					break;
+					
+				case "-n":
+				case "--number":
+					key = "number";
+					break;
+				
+				default:
+					if (key != null) {
+						if (key.equals("number")) {
+							try {
+								Integer.parseInt(args[i].trim());
+							} catch (Exception e) {
+								printErrorMessage("文件起始编号必须是整数。\n" + e.getLocalizedMessage());
+								System.exit(-1);
+							}
+						}
+						results.put(key, args[i].trim());
+						key = null;
+					} else {
+						System.out.println("未知参数: " + args[i].trim());
+						System.exit(-2);
+					}
+					break;
+			}
+			
+		}
+		return results;
+	}
+	
+	private static void printErrorMessage(String msg) {
+		System.err.println("错误: " + msg);
+		System.err.println("");
+	}
+	
+	private static void printDebugMessage(String msg) {
+		if (DEBUG) System.out.println(msg);
+	}
+	
+	private static void printArrayList(String title, ArrayList<File> files) {
+		if (DEBUG) {
+			System.out.println("\n" + title);
+			for (int i = 0; i < files.size(); i++) {
+				System.out.println(files.get(i).getName());
+			}
+			System.out.println("");
+		}
+	}
+}
